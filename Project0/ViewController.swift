ce3eda1a16c899459701dafce8474356c2a891d9
Cynthia <ucbdiaz2015@berkeley.edu>
2015-08-02 12:54:00 -0700

    Initial Commit

---
+++ Project0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project0
+//
+//  Created by ddd on 8/2/15.
+//  Copyright © 2015 Cynthia Diaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
